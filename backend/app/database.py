c269d3d51420b088e6aafdb991e6c2a686b07e5c
CRRINCO <epam.rincon@mbusa.com>
2024-04-30 23:09:06 -0500

    Update database configuration and Azure Key Vault integration in backend

--- backend/app/database.py
+++ backend/app/database.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 if os.getenv("ENVIRONMENT") == "production":
     kv_client = SecretClient(
         vault_url=os.getenv("AZURE_KEYVAULT_RESOURCEENDPOINT"),
-        credential=DefaultAzureCredential(),
+        credential=DefaultAzureCredential(managed_identity_client_id=os.getenv("AZURE_MANAGED_IDENTITY_CLIENT_ID"),
     )
     DATABASE_URL = kv_client.get_secret("db-connection-string")
     engine = create_engine(DATABASE_URL)

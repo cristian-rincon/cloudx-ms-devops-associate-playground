823e8b18f0face42b70d69404bdf7ff75472d001
CRRINCO <epam.rincon@mbusa.com>
2024-05-02 10:36:45 -0500

    Refactor frontend routes and dependencies

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -1,9 +1,60 @@
-var express = require('express');
-var router = express.Router();
+// routes/thoughts.js
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const axios = require("axios");
+const express = require("express");
+
+const bodyParser = require("body-parser");
+const router = express.Router();
+
+const backendUrlBase =
+  "https://cloudx-backend-app-820548440a04bd6e.azurewebsites.net";
+const appTitle = "Thoughts App";
+let thoughts = ["Thought 1", "Thought 2", "Thought 3"]; // This data typically comes from your database.
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+router.get("/", function (req, res) {
+  axios
+    .get(`${backendUrlBase}/thoughts`)
+    .then(function (response) {
+      const thoughts =
+        response.data.length > 0
+          ? response.data.map((thought) => thought.thought).reverse()
+          : ["Hey, no thoughts stored yet!"];
+      res.render("index", { thoughts: thoughts, title: appTitle,  justPosted: req.session.justPosted });
+      req.session.justPosted = false;
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.render("index", {
+        thoughts: ["Hey, no thoughts stored yet!"],
+        title: appTitle,
+        justPosted: req.session.justPosted,
+      });
+      req.session.justPosted = false;
+    });
+});
+
+router.post("/", function (req, res) {
+  let newThought = req.body.content;
+  thoughts.push(newThought);
+  console.log(newThought);
+  axios
+    .post(`${backendUrlBase}/thoughts`, {
+      thought: newThought,
+    })
+    .then(function(response) {
+      console.log(response.data);
+      req.session.justPosted = true;
+      res.redirect('/');
+    })
+    .catch(function(error) {
+      console.log(error);
+      res.redirect('/');
+    });
+
+  // res.redirect('/');
 });
 
 module.exports = router;
